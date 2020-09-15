7648f968ec140b0db1664eca62c90f836b59da0b
anmollimje <softwaredev31@gmail.com>
2020-09-15 08:52:00 +0530

    Done

---
+++ Practical17/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical17
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] arr = new string[2] { "Rock", "John" };
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Response.Write(arr[i]);
+                }
+                catch (IndexOutOfRangeException exc)
+                {
+                    Response.Write("</br>"+exc.Message);
+                }
+            }
+        }
+    }
+}
